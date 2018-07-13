73a69f002d0734b17ceb6c9a95277833aed91b9a
Sebastian Hirsch <sebastian@Linux-Schleppi.localdomain>
2018-07-13 12:44:42 +0200

    project: install jquery and import it

--- yourchoice/src/index.js
+++ yourchoice/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import $ from "jquery";
 import '../node_modules/popper.js/dist/umd/popper.min.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
