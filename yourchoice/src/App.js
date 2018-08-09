5189420584b873e380074ed2f674b85e94518f85
Sebastian Hirsch <sebastian@Linux-Schleppi.localdomain>
2018-08-09 19:01:15 +0200

    front: implement fullpage example.

--- yourchoice/src/App.js
+++ yourchoice/src/App.js
@@ -9,20 +9,40 @@ import OffCanvasNavigation from './Components/OffCanvasNavigation';
 import ContentLeft from './Components/ContentLeft';
 import ContentRight from './Components/ContentRight';
 import Footer from './Components/Footer';
+import {SectionsContainer, Section} from 'react-fullpage';
 
+
+let options = {
+    sectionClassName:     'section',
+    anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
+    scrollBar:            false,
+    navigation:           true,
+    verticalAlign:        false,
+    sectionPaddingTop:    '50px',
+    sectionPaddingBottom: '50px',
+    arrowNavigation:      true
+};
 
 class App extends Component {
   render() {
     return (
         <div className="App">
             <header className="App-header">
-
+                <a href="#sectionOne" className="opa">Section One</a>
+                <a href="#sectionTwo">Section Two</a>
+                <a href="#sectionThree">Section Three</a>
             </header>
             <main>
-
+                <SectionsContainer className="container" {...options}>
+                    <Section className="custom-section" verticalAlign="true" color="#69D2E7">Page 1</Section>
+                    <Section color="#A7DBD8">Page 2</Section>
+                    <Section color="#E0E4CC">Page 3</Section>
+                </SectionsContainer>
             </main>
             <footer>
-
+                <a href="" className="opa">Dcoumentation</a>
+                <a href="">Example Source</a>
+                <a href="">About</a>
             </footer>
         </div>
     );
--- yourchoice/src/index.js
+++ yourchoice/src/index.js
@@ -8,5 +8,6 @@ import '../node_modules/popper.js/dist/umd/popper.min.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
