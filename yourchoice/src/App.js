e7c8bd738eb88531db1972e8cfad465d58eb0c72
DHofmann <daniela.hofmann3010@gmail.com>
2018-08-09 22:08:40 +0200

    front: added sections for content with background-numbers.

--- yourchoice/src/App.js
+++ yourchoice/src/App.js
@@ -14,13 +14,13 @@ import {SectionsContainer, Section} from 'react-fullpage';
 
 let options = {
     sectionClassName:     'section',
-    anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
+    anchors:              ['sectionOne', 'sectionTwo', 'sectionThree', 'sectionFour', 'sectionFive', 'sectionSix'],
     scrollBar:            false,
     navigation:           true,
     verticalAlign:        false,
     sectionPaddingTop:    '50px',
     sectionPaddingBottom: '0px',
-    arrowNavigation:      true
+    arrowNavigation:      false,
 };
 
 class App extends Component {
@@ -31,6 +31,7 @@ class App extends Component {
                 <a href="#sectionOne" className="opa">Section One</a>
                 <a href="#sectionTwo">Section Two</a>
                 <a href="#sectionThree">Section Three</a>
+                <OffCanvas />
             </header>
             <main>
                 <SectionsContainer className="container" {...options}>
@@ -40,8 +41,23 @@ class App extends Component {
                             <h1 className="hero-headline">mehr als nur werbung.</h1>
                         </div>
                     </Section>
-                    <Section></Section>
-                    <Section>Page 3</Section>
+                    <Section>
+                        <p className="background-number number-top-right">1</p>
+                        <ContentLeft />
+                    </Section>
+                    <Section>
+                        <p className="background-number number-top-left">2</p>
+                    </Section>
+                    <Section>
+                        <p className="background-number number-bottom-right">3</p>
+                    </Section>
+                    <Section>
+                        <p className="background-number number-top-left">4</p>
+                    </Section>
+                    <Section>
+                        <p className="background-number number-top-right">5</p>
+                    </Section>
+
                 </SectionsContainer>
             </main>
             <footer>
