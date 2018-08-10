fad610bfc9241478b344af973b460b659bd84b8f
DHofmann <daniela.hofmann3010@gmail.com>
2018-08-10 21:16:41 +0200

    front: added functionality for closing and opening offcanvas.

--- yourchoice/src/Components/OffCanvas.js
+++ yourchoice/src/Components/OffCanvas.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import $ from 'jquery';
 import image from '../img/offcanvas.png';
 import menu from '../img/menu-offcanvas.svg';
 
+function closeOffcanvas() {
+    let offcanvas = document.getElementsByClassName('offcanvas');
+    $(offcanvas).fadeOut({queue: false, duration: 'slow'});
+    $(offcanvas).animate({top: '-100%', opacity: '0'}, 350,function () {
+         $(offcanvas).css({
+             top: '-100%',
+             opacity: '0',
+         });
+     });
+}
 
 const offcanvas = () => {
     return (
         <div className="offcanvas">
             <div className="relative">
-                <div className="offcanvas-menu">
+                <div className="offcanvas-menu" onClick={closeOffcanvas}>
                     <img src={menu} alt="Menü" className=""/>
                 </div>
                 <img src={image} alt="It's your choice" className="offcanvas-image"/>
                 <div className="offcanvas-nav">
                     <a href="https://www.google.de" className="offcanvas-link">die agentur.</a>
-                    <a href="#section1" className="offcanvas-link">die storyline.</a>
+                    <a href="#section1" className="offcanvas-link" onClick={closeOffcanvas}>die storyline.</a>
                 </div>
             </div>
         </div>
--- yourchoice/src/Components/OffCanvasNavigation.js
+++ yourchoice/src/Components/OffCanvasNavigation.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import $ from 'jquery';
 import logo from '../img/logo.svg';
 import menu from '../img/menu.svg';
 
+function openOffcanvas() {
+    let offcanvas = document.getElementsByClassName('offcanvas');
+    $(offcanvas).fadeIn({queue: false, duration: 'slow'});
+    $(offcanvas).animate({top: '0', opacity: '1'}, 350, function () {
+        $(offcanvas).css({
+            top: '0',
+            opacity: '1',
+        });
+    });
+}
 
 const offcanvasNavigation = () => {
     return (
         <div>
             <img src={logo} alt="Logo" className="logo"/>
-            <div className="menu">
+            <div className="menu" onClick={openOffcanvas}>
                 <img src={menu} alt="Menü" className=""/>
             </div>
         </div>
