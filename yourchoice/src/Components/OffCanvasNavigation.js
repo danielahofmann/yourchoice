d1dcb83676a571f0b46140ea0e270bc3c4e7c644
DHofmann <daniela.hofmann3010@gmail.com>
2018-08-10 19:10:20 +0200

    front: added offcanvasNav and styled it.

--- yourchoice/src/Components/OffCanvasNavigation.js
+++ yourchoice/src/Components/OffCanvasNavigation.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../img/logo.svg';
+import menu from '../img/menu.svg';
+
 
 const offcanvasNavigation = () => {
     return (
         <div>
-            <p>this is the nav of the offcanvas</p>
+            <img src={logo} alt="Logo" className="logo"/>
+            <div className="menu">
+                <img src={menu} alt="Menü" className=""/>
+            </div>
         </div>
     )
 };
