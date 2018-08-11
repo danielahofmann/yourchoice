6536007d737ef684e2d2bbd76cdc478864f96efb
Sebastian Hirsch <sebastian@Linux-Schleppi.localdomain>
2018-08-11 13:09:27 +0200

    Add asic youtube-player to FullFrame

--- yourchoice/src/Components/VideoFullFrame.js
+++ yourchoice/src/Components/VideoFullFrame.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
+import { X } from 'react-feather';
 
 class VideoFullFrame extends Component {
     state = {
@@ -6,9 +8,25 @@ class VideoFullFrame extends Component {
     }
 
     render() {
+        const opts = {
+            height: '100%',
+            width: '100%',
+            playerVars: {
+                autoplay: 1,
+                hl: de
+            }
+        };
+
         return (
             <div className="VideoFullFrame">
-
+                <button className="icon-button">
+                    <X />
+                </button>
+                <YouTube
+                    videoId="61yrZiWPXsQ"
+                    opts={opts}
+                    onReady={this._onReady}
+                />
             </div>
         );
     }
