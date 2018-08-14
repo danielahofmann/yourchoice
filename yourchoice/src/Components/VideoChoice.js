8ca53c85276b7b035f5c0f6557348e9be3f58e68
Sebastian Hirsch <sebastian@Linux-Schleppi.localdomain>
2018-08-14 17:19:41 +0200

    front: add animation and alt-tags

--- yourchoice/src/Components/VideoChoice.js
+++ yourchoice/src/Components/VideoChoice.js
@@ -26,9 +26,9 @@ class VideoChoice extends Component {
             <div className="VideoChoice hide">
                 <audio src={mp3} autoload/>
                 <div className="image-container">
-                    <input type="image" src={this.props.imageHip} id='1' onClick={this.props.click} className="visionärButton hover" />
+                    <input type="image" src={this.props.imageHip} alt="Visionär" id='1' onClick={this.props.click} className="visionärButton hover" />
 
-                    <input type="image" src={this.props.imagePrag} id='2' onClick={this.props.click}className="pragmatikerButton hover" />
+                    <input type="image" src={this.props.imagePrag} alt="Pragmatiker" id='2' onClick={this.props.click}className="pragmatikerButton hover" />
                 </div>
             </div>
         );
