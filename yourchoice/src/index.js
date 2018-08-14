747e10e9b70bf9fc094ee3c3b660b387557c37db
DHofmann <daniela.hofmann3010@gmail.com>
2018-08-14 18:10:23 +0200

    build: added build params and deleted registerServiceWorker

--- yourchoice/src/index.js
+++ yourchoice/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import $ from "jquery";
 import '../node_modules/popper.js/dist/umd/popper.min.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -10,4 +9,3 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
